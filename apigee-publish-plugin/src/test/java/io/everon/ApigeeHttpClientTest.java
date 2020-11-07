32e58d5724c27e461ef30fe8ec769e1d57c2cf89
Jovan Kricka <jovan.kricka@ev-box.com>
2020-11-07 19:23:46 +0100

    Include test cases for obtaining access token

--- apigee-publish-plugin/src/test/java/io/everon/ApigeeHttpClientTest.java
+++ apigee-publish-plugin/src/test/java/io/everon/ApigeeHttpClientTest.java
@@ -1,20 +1,93 @@
 package io.everon;
 
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.junit.jupiter.MockServerExtension;
+import org.mockserver.junit.jupiter.MockServerSettings;
+
+import java.net.ConnectException;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockserver.matchers.Times.exactly;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
+@ExtendWith(MockServerExtension.class)
+@MockServerSettings(ports = {8787})
 class ApigeeHttpClientTest {
 
     private static final String ORGANIZATION_NAME = "organization";
     private static final String PORTAL_NAME = "portal";
 
+    private MockServerClient mockServerClient;
     private final ApigeeHttpClient client = new ApigeeHttpClient(ORGANIZATION_NAME, PORTAL_NAME);
 
-    @Test
-    void basicTest() {
+    @BeforeEach
+    public void setup(MockServerClient mockServerClient) {
+        this.mockServerClient = mockServerClient;
+    }
 
-        assertThat(client).isNotNull();
+    @Test
+    void obtainApigeeAccessTokenSuccessfully() {
+
+        String accessToken = "You shall pass!";
+        String basicAuthToken = UUID.randomUUID().toString();
+        client.setLoginUrl("http://127.0.0.1:8787/oauth/token");
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/oauth/token")
+                        .withHeader("Authorization", "Basic " + basicAuthToken), exactly(1))
+                .respond(response()
+                        .withStatusCode(HttpStatus.SC_OK)
+                        .withBody("{ \"access_token\": \"" + accessToken + "\" }"));
+
+        String fetchedAcessToken =
+                client.obtainApigeeAccessToken("chucknorris", "chucknorris", basicAuthToken);
+
+        assertThat(fetchedAcessToken).isEqualTo(accessToken);
+
+    }
+
+    @Test
+    void obtainApigeeAccessTokenConnectionBrokenExceptionThrown() {
+
+        String basicAuthToken = UUID.randomUUID().toString();
+        client.setLoginUrl("http://127.0.0.1:8787/oauth/token");
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/oauth/token")
+                        .withHeader("Authorization", "Basic " + basicAuthToken), exactly(1))
+                .respond(response -> {
+                    throw new ConnectException("Connection exploded!");
+                });
+
+        assertThrows(RuntimeException.class,
+                () -> client.obtainApigeeAccessToken("chucknorris", "chucknorris", basicAuthToken));
+
+    }
+
+    @Test
+    void obtainApigeeAccessTokenNonSuccessStatusReturnedExceptionThrown() {
+
+        String basicAuthToken = UUID.randomUUID().toString();
+        client.setLoginUrl("http://127.0.0.1:8787/oauth/token");
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/oauth/token")
+                        .withHeader("Authorization", "Basic " + basicAuthToken), exactly(1))
+                .respond(response()
+                        .withStatusCode(HttpStatus.SC_BAD_REQUEST));
+
+        assertThrows(RuntimeException.class,
+                () -> client.obtainApigeeAccessToken("chucknorris", "chucknorris", basicAuthToken));
 
     }
 
